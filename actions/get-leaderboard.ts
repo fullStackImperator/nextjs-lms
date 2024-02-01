eb79290dcb1131eb1bb4044571fc869ef770f494
fullStackImperator <stevanv@gmx.de>
2024-02-01 23:09:34 +0100

    feat(grading): added grading for students per course update

--- actions/get-leaderboard.ts
+++ actions/get-leaderboard.ts
@@ -1,12 +1,15 @@
 import { Category, Course } from '@prisma/client'
+
 import { getProgress } from '@/actions/get-progress'
 import { db } from '@/lib/db'
 
 type Leaderboard = {
-  [userId: string]: number; // Define the shape of the leaderboard object
+  userId: string
+  totalPoints: number
 }
 
-export const getLeaderboard = async (): Promise<Leaderboard[]> => {
+
+export const getLeaderboard = async () => {
   try {
     const gradings = await db.grading.findMany({
       select: {
@@ -15,31 +18,31 @@ export const getLeaderboard = async (): Promise<Leaderboard[]> => {
       },
     })
 
-    const leaderboard: Leaderboard = {}; // Initialize leaderboard as an empty object
+    const leaderboard = {}
 
     gradings.forEach((grading) => {
-      const userId = grading.userId;
-      const points = grading.points;
+      const userId = grading.userId
+      const points = grading.points
 
       if (leaderboard[userId]) {
-        leaderboard[userId] += points; // Increment points if userId exists in the leaderboard
+        leaderboard[userId] += points
       } else {
-        leaderboard[userId] = points; // Otherwise, set points for the userId
+        leaderboard[userId] = points
       }
-    });
+    })
 
     // Convert leaderboard object to an array of objects
     const leaderboardArray = Object.keys(leaderboard).map((userId) => ({
       userId,
       totalPoints: leaderboard[userId],
-    }));
+    }))
 
     // Sort the leaderboard by totalPoints in descending order
-    leaderboardArray.sort((a, b) => b.totalPoints - a.totalPoints);
+    leaderboardArray.sort((a, b) => b.totalPoints - a.totalPoints)
 
-    return leaderboardArray as Leaderboard[]; // Convert to Leaderboard[] and return
+    return leaderboardArray
   } catch (error) {
-    console.log('[GET_LEADERBOARD]', error);
-    return [];
+    console.log('[GET_LEADERBOARD]', error)
+    return []
   }
-};
+}
