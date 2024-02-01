b6df07f4e3a0a03e56ff0a977c1b26b5c5972c58
fullStackImperator <stevanv@gmx.de>
2024-02-01 22:57:15 +0100

    feat(grading): added grading for students per course update

--- actions/get-leaderboard copy.ts
+++ actions/get-leaderboard copy.ts
@@ -1,48 +1,48 @@
-import { Category, Course } from '@prisma/client'
+// import { Category, Course } from '@prisma/client'
 
-import { getProgress } from '@/actions/get-progress'
-import { db } from '@/lib/db'
+// import { getProgress } from '@/actions/get-progress'
+// import { db } from '@/lib/db'
 
-type Leaderboard = {
-  userId: string
-  totalPoints: number
-}
+// type Leaderboard = {
+//   userId: string
+//   totalPoints: number
+// }
 
 
-export const getLeaderboard = async (): Promise<Leaderboard[]> => {
-  try {
-    const gradings = await db.grading.findMany({
-      select: {
-        userId: true,
-        points: true,
-      },
-    })
+// export const getLeaderboard = async (): Promise<Leaderboard[]> => {
+//   try {
+//     const gradings = await db.grading.findMany({
+//       select: {
+//         userId: true,
+//         points: true,
+//       },
+//     })
 
-    const leaderboard = {}
+//     const leaderboard = {}
 
-    gradings.forEach((grading) => {
-      const userId = grading.userId
-      const points = grading.points
+//     gradings.forEach((grading) => {
+//       const userId = grading.userId
+//       const points = grading.points
 
-      if (leaderboard[userId]) {
-        leaderboard[userId] += points
-      } else {
-        leaderboard[userId] = points
-      }
-    })
+//       if (leaderboard[userId]) {
+//         leaderboard[userId] += points
+//       } else {
+//         leaderboard[userId] = points
+//       }
+//     })
 
-    // Convert leaderboard object to an array of objects
-    const leaderboardArray = Object.keys(leaderboard).map((userId) => ({
-      userId,
-      totalPoints: leaderboard[userId],
-    }))
+//     // Convert leaderboard object to an array of objects
+//     const leaderboardArray = Object.keys(leaderboard).map((userId) => ({
+//       userId,
+//       totalPoints: leaderboard[userId],
+//     }))
 
-    // Sort the leaderboard by totalPoints in descending order
-    leaderboardArray.sort((a, b) => b.totalPoints - a.totalPoints)
+//     // Sort the leaderboard by totalPoints in descending order
+//     leaderboardArray.sort((a, b) => b.totalPoints - a.totalPoints)
 
-    return leaderboardArray
-  } catch (error) {
-    console.log('[GET_LEADERBOARD]', error)
-    return []
-  }
-}
+//     return leaderboardArray
+//   } catch (error) {
+//     console.log('[GET_LEADERBOARD]', error)
+//     return []
+//   }
+// }
