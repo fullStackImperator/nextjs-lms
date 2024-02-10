fd224fe3da41de0664195a407401938b1722af4c
Stevan Veljovic <stevanv@gmx.de>
2024-02-10 01:07:08 +0100

    3 teachers workaround

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,4 +1,3 @@
-
 const teacherIds = [
   process.env.NEXT_PUBLIC_TEACHER_ID,
   process.env.NEXT_PUBLIC_TEACHER_ID2,
@@ -6,19 +5,19 @@ const teacherIds = [
   process.env.NEXT_PUBLIC_TEACHER_ID4,
 ]
 
-// export const isTeacher = (userId?:string | null) => {
-//     return (
-//         teacherIds.includes(userId)
-//         // userId === process.env.NEXT_PUBLIC_TEACHER_ID ||
-//         // userId === process.env.NEXT_PUBLIC_TEACHER_ID2 ||
-//         // userId === process.env.NEXT_PUBLIC_TEACHER_ID3 ||
-//         // userId === process.env.NEXT_PUBLIC_TEACHER_ID4
-//     )
-// }
-
-export const isTeacher = (userId: string | null | undefined): boolean => {
-  if (userId === null || userId === undefined) {
-    return false // If userId is null or undefined, it's not a teacher
-  }
-  return teacherIds.includes(userId)
+export const isTeacher = (userId?: string | null) => {
+  return (
+    // teacherIds.includes(userId)
+    userId === process.env.NEXT_PUBLIC_TEACHER_ID ||
+    userId === process.env.NEXT_PUBLIC_TEACHER_ID2 ||
+    userId === process.env.NEXT_PUBLIC_TEACHER_ID3 ||
+    userId === process.env.NEXT_PUBLIC_TEACHER_ID4
+  )
 }
+
+// export const isTeacher = (userId: string | null | undefined): boolean => {
+//   if (userId === null || userId === undefined) {
+//     return false // If userId is null or undefined, it's not a teacher
+//   }
+//   return teacherIds.includes(userId)
+// }
